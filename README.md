9b437d32c738c8d1955e2d4df89e3e04725a0915
ShawnX <xwxu@astreetpress.com>
2013-03-19 14:05:11 +0800

    implemented

---
+++ README.md
@@ -0,0 +1,35 @@
+# jQuery mousedrag plugin/event
+
+Simulate mouse dragging event(mousemove while mousedown).
+
+## Usage
+
+* as a plugin
+
+    $('#foo').mousedrag(function(ev) { // mouse being dragged });
+
+    $('#foo').mousedrag(
+        function(ev) {  // mouse being dragged }, 
+        {  
+            // optional
+            start: function(ev) { // drag is started  },
+            stop: function(ev) { // drag is stopped }
+        }
+    );
+    
+* as a special event
+
+    $('#foo').on('mousedrag', function(ev) { // mouse being dragged });
+    
+    $('#foo').on(
+        'mousedrag',
+        {
+            start: function(ev) {},
+            stop: function(ev) {}
+        },
+        function(ev) {}
+    );
+
+## dependencies
+
+jquery 1.4+ 
---
+++ jquery-mousedrag.js
@@ -0,0 +1,61 @@
+(function($) {
+
+    $.fn.mousedrag = function() {
+
+        var that = this;
+
+        if (arguments[0] == 'destroy') {
+            this.off('mousedown.md mouseup.md mousemove.md mouseleave.md');
+            return this;
+        }
+
+        var // is mouse button held down?
+            isMouseDown = false, 
+            // has dragging started
+            started = false, 
+            // the dragging handler
+        	dragHandler = $.isFunction(arguments[0]) ? arguments[0] : null, 
+            // extra handlers/options
+            // data.start() and data.stop() are available
+            data = $.isPlainObject(arguments[1]) ? arguments[1] : null;
+
+        this
+            .on('mousemove.md', function() {
+                if ( isMouseDown ) {
+                    if (!started) {
+                        // dragging started
+                        $.isFunction(data.start) && data.start.apply(that, arguments);
+                    }
+                    // being dragged
+                    dragHandler && dragHandler.apply(that, arguments);
+                    started = true;
+                }
+
+            })
+            .on('mousedown.md', function() {
+                isMouseDown = true;
+            })
+            .on('mouseup.md mouseleave.md', function() {
+                if (started) {
+                    // dragging stopped
+                    $.isFunction(data.stop) && data.stop.apply(that, arguments);
+                }
+                isMouseDown = false;
+                started = false;
+            });
+
+        return this;
+    };
+
+    $.event.special.mousedrag = {
+        add: function(handlerObj) {
+            $(this).mousedrag(handlerObj.handler, handlerObj.data);
+            return false;
+        },
+        remove: function() {
+            $(this).mouseDraggable('destroy');            
+            return false;
+        }
+    };
+
+})(jQuery);
